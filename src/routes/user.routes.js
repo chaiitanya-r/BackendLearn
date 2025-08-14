e6fa0e1cf0c3aebc1881fc72c4a592046a1301e5
Chaitanya Retawade <chaitanya.retawade08@gmail.com>
2025-08-14 19:41:26 +0530

    refactor user routes to organize imports and fix route definitions

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import {
+    changeCurrentPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
+    loginUser,
+    logoutUser,
+    registerUser,
+    updateUserAvatar,
+    updateUserProfile
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { refreshAccessToken } from "../controllers/user.controller.js";
@@ -22,5 +32,19 @@ router.route('/login').post(loginUser)
 router.route('/logout').post(verifyJWT, logoutUser) // Define a route for user logout that requires JWT verification
 
 router.route('/refresh-token').post(refreshAccessToken)
+
+router.route('change-password').post(verifyJWT, changeCurrentPassword)
+
+router.route('/current-user').get(verifyJWT, getCurrentUser)
+
+route.route('/update-account').patch(verifyJWT, updateUserProfile)
+
+router.route('/avatar').patch(verifyJWT, upload.single('avatar'), updateUserAvatar)
+
+router.route('/cover-image').patch(verifyJWT, upload.single('coverImage'), updateUserAvatar)
+
+router.routes('/c/:username').get(verifyJWT, getUserChannelProfile)
+
+router.route('/history').get(verifyJWT, getWatchHistory)
 
 export default router;
